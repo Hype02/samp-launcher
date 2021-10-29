06ab27f447128395581c89fd2f8cd29679130953
Hype02 <kontakt.sebastian.klawikowski@gmail.com>
2021-10-29 22:11:10 +0200

    Will come back to this later

--- src/frontend/main.ts
+++ src/frontend/main.ts
@@ -9,8 +9,32 @@ const app = new App({
   },*/
 });
 
+
+class ServerInfo{
+
+}
+
+ip    "194.5.178.83:7777"
+hn    "Atlas Roleplay - Status: Online"
+pc    1
+pm    50
+gm    "RP/RPG/TDM/DM/GANG/CNR"
+la    "-"
+pa    false
+vn    "0.3.7-R2"
+
 class SampApi {
+  constructor (){
+
+  }
+
+  GetAllServersList(): ServerInfo[]{
   
+  }
+
+  GetPublicServers
+
+
 }
 
 export default app;
