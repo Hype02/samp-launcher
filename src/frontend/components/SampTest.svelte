783f1ca08b07b2b0c6856861411233f7242292d7
Hype02 <kontakt.sebastian.klawikowski@gmail.com>
2021-10-30 14:42:45 +0200

    .

--- src/frontend/components/SampTest.svelte
+++ src/frontend/components/SampTest.svelte
@@ -3,10 +3,12 @@
 </style>
 
 <script>
-import {exec} from '@xarc/run'
+import {exec} from '../../../node_modules/run'
 
 try{
-    exec(`wine "/home/hjp/.local/share/Steam/steamapps/common/Grand Theft Auto San Andreas"`)
+
+    exec(`wine "/home/hjp/.local/share/Steam/steamapps/common/Grand Theft Auto San Andreas/gta-sa.exe"`)
+    console.log("done")
 }
 catch(err){
     console.log(err)
